d640369342b1418976f070ba7ee3b16cb9432a55
Davy Felix <davydesouza02@gmail.com>
2026-02-04 22:23:52 -0300

    Initial commit

---
+++ app/src/main/java/br/davyfelix/dinofoods/services/FirebaseConfiguration.kt
@@ -0,0 +1,27 @@
+package br.davyfelix.dinofoods.services
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+object FirebaseConfig {
+
+    private var databaseReference: DatabaseReference? = null
+    private var firebaseAuth: FirebaseAuth? = null
+
+    @JvmStatic
+    fun getFirebaseDatabase(): DatabaseReference {
+        if (databaseReference == null) {
+            databaseReference = FirebaseDatabase.getInstance().reference
+        }
+        return databaseReference!!
+    }
+
+    @JvmStatic
+    fun getFirebaseAuth(): FirebaseAuth {
+        if (firebaseAuth == null) {
+            firebaseAuth = FirebaseAuth.getInstance()
+        }
+        return firebaseAuth!!
+    }
+}
